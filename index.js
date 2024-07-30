0d3210eef64c84ab96391568575529493a8d4a88
ojokotor <161373795+ojokotor@users.noreply.github.com>
2024-07-30 02:20:50 +0100

    Update index.js
    Removed the first declaration of port

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const basicAuth = require('basic-auth');
 const axios = require('axios');
 
 const app = express();
-const port = 3000;
+// const port = 3000;
 
 // Middleware to parse JSON
 app.use(bodyParser.json());
